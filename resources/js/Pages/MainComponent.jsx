dda5c41302fec03a8d2b32de5412186f5cc49163
Yuya <obelu1001@gmail.com>
2025-07-08 13:57:37 +0900

    20250708-プロジェクト編集後、経歴内の関連プロジェクトデータも即時反映される。

--- resources/js/Pages/MainComponent.jsx
+++ resources/js/Pages/MainComponent.jsx
@@ -73,7 +73,7 @@ export default function MainComponent() {
         <ProjectsSection 
           projects={projects}
           setSkills={setSkills}
-          onProjectsUpdate={() => {
+          onProjectsUpdated={() => {
             reloadProjects();
             reloadExperiences();
           }}
